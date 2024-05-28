3c0f790c30e5733932cd876f0a9e78a95734ea45
jesusrj <reginaldo.jesus@gmail.com>
2024-05-28 08:35:30 -0300

    dynamic reflect tests

--- reflection/dynamic_struct/main.go
+++ reflection/dynamic_struct/main.go
@@ -15,18 +15,22 @@ func main() {
 	structFields := []reflect.StructField{
 		{Name: "Name", Type: reflect.TypeOf(""), Tag: reflect.StructTag(`json:"name"`)},
 		{Name: "Age", Type: reflect.TypeOf(9), Tag: reflect.StructTag(`json:"age"`)},
-		{Name: "Address", Type: reflect.TypeOf(""), Tag: reflect.StructTag(`json:"address"`)},
 	}
 
 	definitionType := reflect.StructOf(structFields)
 
+	// With set field
+	value := reflect.New(definitionType)
+	value.Elem().FieldByName("Name").SetString("JESUS")
+	value.Elem().FieldByName("Age").SetInt(43)
+	fmt.Printf("%+v\n", value)
+
+	// With JSON Unmarshal
 	person := reflect.New(definitionType).Interface()
-
-	data := `{"name": "Jesus", "age": 43, "address": "rua do além mar"}`
-
+	data := `{"name": "Miguel", "age": 9, "address": "rua do além mar"}`
 	if err := json.Unmarshal([]byte(data), person); err != nil {
 		log.Fatal(err)
 	}
-
 	fmt.Printf("%+v", person)
+
 }
